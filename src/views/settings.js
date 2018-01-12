c5e54435eaf36fa9836e0c84f7b020b4d7f43637
David Samuel <cryptondave@gmail.com>
2018-01-11 18:40:37 -0800

    fixed build

--- src/views/settings.js
+++ src/views/settings.js
@@ -9,12 +9,6 @@ import Welcome from '../components/balance'
 class Settings extends Component {
 
   render () {
-    function pressBuy(link) {
-      function handleClick(e) {
-        e.preventDefault();
-        console.log(`The ${link} was clicked.`);
-      }
-    }
     return (
             <div className="link-account">
               <Navigation />
@@ -23,14 +17,14 @@ class Settings extends Component {
                   <Welcome name="Woz" balance="$1000.00"/>
                 </p>
               </div>
-                <form onSubmit={this.props.handleClick}>
+                <form>
                   <div className="form-section">
                     <div className="link-bank">
                       <button type="button" value=""><h1>Link Your Bank</h1></button>
                     </div>
                     <div class="link-crypto">
 
-                      <button type="button" onClick={pressBuy} value="BTC"><h2>Buy Bitcoin</h2></button>
+                      <button type="button" value="BTC"><h2>Buy Bitcoin</h2></button>
                       <button type="button" value="ETH"><h2>Buy Ethereum</h2></button>
                       <button type="button" value="LTC"><h2>Buy Litecoin</h2></button>
                       <button type="button" value="XMR"><h2>Buy Monero</h2></button>
