fe01522c6c648cf34a24cbc233aa81ded6c2deec
David Samuel <cryptondave@gmail.com>
2018-01-11 20:53:37 -0800

    added noopener norefresher

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -11,7 +11,7 @@ class Navigation extends Component {
           <Link to="/"><img src={icon} className="App-logo" alt="logo"></img></Link>
         </ul>
         <ul >
-          <a href="https://goo.gl/forms/zcKQQaNuS3qL98yu2" target="_blank">
+          <a href="https://goo.gl/forms/zcKQQaNuS3qL98yu2" target="_blank" rel="noopener noreferrer">
             <button className="feedback-button">
               <span>
                   <p>Click to leave Feedback <span role="img" aria-labelledby="megaphone"> 📣 </span></p>
