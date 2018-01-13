62482bd444274c2350c151cb772b3116471fe159
David Samuel <cryptondave@gmail.com>
2018-01-13 02:36:38 -0800

    fixed button

--- src/components/signup-form.js
+++ src/components/signup-form.js
@@ -4,16 +4,25 @@ import '../App.css'
 
 class SignUpForm extends Component {
   render() {
-    var register = {"border": "1px solid black", "width": "150px"}
+    var register = {"border": "1px solid black", "width": "150px", "backgroundColor": "white"}
     var signup = {"paddingTop": "10px"}
     return (
       <div className="signup-form">
-        <h3>Let CryptoBox manage your cryptocurrency</h3>
-          <form className='signup-form'>
-              <div style={signup}>
-                <Link to="/settings"><button style={register} type='submit'><h3>Sign Up</h3></button></Link>
-              </div>
-          </form>
+        <section>
+          <header>
+            <h3>Let CryptoBox manage your cryptocurrency</h3>
+          </header>
+
+          <div>
+            <form className='signup-form'>
+                <div style={signup}>
+                  <Link to="/settings"><button style={register} type='submit'><h3>Try It Free!</h3></button></Link>
+                </div>
+            </form>
+          </div>
+
+        </section>
+
         </div>
         );
   }
