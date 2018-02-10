5def0f01460a0cb49e5f420d6d0bfaa96d61d804
David Samuel <cryptondave@gmail.com>
2018-02-10 13:33:28 -0800

    changed calculator layout, not constantly updating

--- src/components/select.js
+++ src/components/select.js
@@ -130,7 +130,7 @@ export default class Select extends Component {
     const cyanBorder = {"border": "1px solid white", "color": "white"}
     // const paddingSides = {"paddingRight": "5px", "paddingLeft": "5px"}
 
-    const isInvalid = this.state.amount === 0 || this.state.fromType === '' || this.state.toType === '' || this.state.fromType === this.state.toType;
+    const isInvalid = this.state.amount === '' || this.state.fromType === '' || this.state.toType === '' || this.state.fromType === this.state.toType;
 
     const myPrices = round(this.state.amount, 2).toLocaleString("currency");
     const price1 = round(prices[this.state.fromType], 6);
@@ -252,7 +252,7 @@ export default class Select extends Component {
            style={cyanBorder}
            hidden={disabled}
          />
-         <button disabled={isInvalid} className="btn btn-outline-light input-group-append"  type="submit" style={{"width": "40%"}}>
+         <button disabled={isInvalid} className="btn btn-success input-group-append"  type="submit" style={{"width": "40%"}}>
            <i className="fa fa-bolt"></i>{' Trade $' + myPrices}
          </button>
 
