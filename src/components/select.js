96d04175c744d2aa501b2d097ee47adc95976d99
David Samuel <cryptondave@gmail.com>
2018-02-10 17:00:55 -0800

    round dollar amt to 8

--- src/components/select.js
+++ src/components/select.js
@@ -163,7 +163,7 @@ export default class Select extends Component {
           <input
              placeholder={'$'+round(this.state.amount, 2)} onChange={e => this.inputChanged()} min={0}
              className=" container bg-dark text-light form-control" type="number" ref={ el => this.dollar = el }
-           style={cyanBorder} value={this.state.amount}
+           style={cyanBorder} value={round(this.state.amount, 8)}
          />
 
             <select  className=" input-group-append form-control" defaultValue="btc" onChange={this.handleFromChange} >
